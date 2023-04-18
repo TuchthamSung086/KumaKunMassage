149a41d75d3d1414bb84799f8ed21b9a281a14ae
Tuchtham Sungkameka <touch0869@gmail.com>
2023-04-18 11:34:51 +0700

    feat(controllers): get all massage shops

--- controllers/massageShops.js
+++ controllers/massageShops.js
@@ -3,15 +3,19 @@ const MassageShop = require('../models/MassageShop.js');
 //@desc     Get vaccine centers
 //@router   GET /api/v1/hospitals/vecCenters
 //@access   Public
-exports.getMassageShops = (req, res, next) => {
-    MassageShop.getAll((err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occured while retrieving Massage Shops."
-            });
-        else res.send(data);
-    })
+exports.getMassageShops = async (req, res, next) => {
+    try {
+        const massageShops = await MassageShop.find();
+        res.status(200).json({ success: true, data: massageShops });
+    }
+    catch (err) {
+        res.status(500).send({
+            message:
+                err.message || "Some error occured while retrieving Massage Shops."
+        });
+    }
+
+
 };
 
 //@desc     Create new hospital
