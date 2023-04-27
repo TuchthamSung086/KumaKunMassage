a4e653f94ea5ca51b9705af403691035e380cb39
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-04-27 13:50:53 +0700

    Revert "commits"
    This reverts commit 8ad475e7dad79b576fd8834ab0d5309c09fdc2d2.

--- routes/appointments.js
+++ routes/appointments.js
@@ -4,15 +4,15 @@ const { getAppointments, getAppointment, addAppointment, updateAppointment, dele
 
 const router = express.Router({ mergeParams: true });
 
-const { protect, authorize } = require('../middleware/auth');
+const { protect, verified, authorize } = require('../middleware/auth');
 
 router.route('/')
     .get(protect, getAppointments)
-    .post(protect, authorize('admin', 'user'), addAppointment);
+    .post(protect, verified(), authorize('admin', 'user'), addAppointment);
 
 router.route('/:id')
     .get(protect, getAppointment)
-    .put(protect, authorize('admin', 'user'), updateAppointment)
-    .delete(protect, authorize('admin', 'user'), deleteAppointment);
+    .put(protect, verified(), authorize('admin', 'user'), updateAppointment)
+    .delete(protect, verified(), authorize('admin', 'user'), deleteAppointment);
 
 module.exports = router;
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -7,14 +7,14 @@ const appointmentRouter = require('./appointments');
 
 const router = express.Router();
 
-const { protect, authorize } = require("../middleware/auth");
+const { protect, verified, authorize } = require("../middleware/auth");
 
 // Re-route into other resource routers
 router.use('/:hospitalId/appointments/', appointmentRouter);
 
 router.route('/vacCenters').get(getVacCenters);
-router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
-router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
+router.route('/').get(getHospitals).post(protect, verified(), authorize('admin'), createHospital);
+router.route('/:id').get(getHospital).put(protect, verified(), authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
 
 module.exports = router;
 
