878905acb42429bdd6e0c6d78a51fa283811b167
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-05-04 18:39:55 +0700

    User resend verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { register, verify, login, getMe, logout } = require('../controllers/auth');
+const { register, verify, login, getMe, logout, resendToken } = require('../controllers/auth');
 
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
 
 router.post('/register', register);
-router.get('/verify/:token', verify)
+router.get('/verify/:token', verify);
+router.post('/verify/resend', protect, resendToken);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
+
 
 module.exports = router;
